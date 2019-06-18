92a2c72d83f8ea42b9f6157fa270a84422aae198
Rishikesh <hramjith@gmail.com>
2019-06-18 14:09:49 +0200

    chapter 8 - API

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,13 +2,41 @@ package com.example.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
+    private val items = listOf(
+        "Mon 6/23 - Sunny - 31/17",
+        "Tue 6/24 - Foggy - 21/8",
+        "Wed 6/25 - Cloudy - 22/17",
+        "Thur 6/26 - Rainy - 22/17",
+        "Fri 6/27 - Foggy - 21/10",
+        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+        "Sun 6/29 - Sunny - 20/7"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
+
+        val forecastList: RecyclerView = find(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&zip=94043&mode=json&units=metric&cnt=7"
+
+        doAsync{
+            Request(url).run()
+            uiThread {longToast("Request performed")}
+        }
     }
+
+
 }
